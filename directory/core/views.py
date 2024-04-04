175f9f566dbd714e880dbb8e5f8d62d1260c80d9
zaki <reborn7875@gmail.com>
2024-04-04 15:54:38 +0800

    feat: Add initial CRUD functionality and Docker setup for PostgreSQL
    Added CRUD functionality to the core app, including migrations, serializers, tasks, and URLs. Also added Docker Compose configuration for setting up PostgreSQL database.

--- directory/core/views.py
+++ directory/core/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Contact
+from .serializers import ContactSerializer, UpdateContactSerializer
 
-# Create your views here.
+
+@api_view(["GET", "POST"])
+def contact_list(request):
+    if request.method == "GET":
+        contacts = Contact.objects.filter(is_deleted=False)
+        serializer = ContactSerializer(contacts, many=True)
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = ContactSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "PATCH", "DELETE"])
+def contact_detail(request, pk):
+    try:
+        contact = Contact.objects.get(pk=pk)
+    except Contact.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = ContactSerializer(contact)
+        return Response(serializer.data)
+
+    elif request.method in ["PUT", "PATCH"]:
+        serializer = UpdateContactSerializer(
+            contact, data=request.data, partial=request.method == "PATCH"
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        contact.soft_delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- directory/directory/settings.py
+++ directory/directory/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'core',
 ]
 
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'directory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Django',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5434',
     }
 }
 
