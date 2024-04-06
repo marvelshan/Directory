c15fd583051b55611a1c522329e2b2c9a214faee
zaki <reborn7875@gmail.com>
2024-04-06 19:58:43 +0800

    style: Change from FBVs to be CBVs
    - chage file views.py
    - change file url.py on injected way

--- directory/core/urls.py
+++ directory/core/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.contact_list),
-    path("<int:pk>/", views.contact_detail),
-    path("search/", views.search_contact),
-    path("export/", views.export_contacts),
+    path("", views.ContactList.as_view(), name="contact_list"),
+    path("<int:pk>/", views.ContactDetail.as_view(), name="contact_detail"),
+    path("search/", views.SearchContact.as_view(), name="search_contact"),
+    path("export/", views.ExportContacts.as_view(), name="export_contacts"),
 ]
--- directory/core/views.py
+++ directory/core/views.py
@@ -1,4 +1,4 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Contact
@@ -6,14 +6,13 @@ from .serializers import ContactSerializer, UpdateContactSerializer
 from .tasks import send_contact_list_email
 
 
-@api_view(["GET", "POST"])
-def contact_list(request):
-    if request.method == "GET":
+class ContactList(APIView):
+    def get(self, request):
         contacts = Contact.objects.filter(is_deleted=False)
         serializer = ContactSerializer(contacts, many=True)
         return Response(serializer.data)
 
-    elif request.method == "POST":
+    def post(self, request):
         serializer = ContactSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -21,54 +20,69 @@ def contact_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["GET", "PUT", "PATCH", "DELETE"])
-def contact_detail(request, pk):
-    try:
-        contact = Contact.objects.get(pk=pk)
-    except Contact.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class ContactDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Contact.objects.get(pk=pk)
+        except Contact.DoesNotExist:
+            raise status.HTTP_404_NOT_FOUND
 
-    if request.method == "GET":
+    def get(self, request, pk):
+        contact = self.get_object(pk)
         serializer = ContactSerializer(contact)
         return Response(serializer.data)
 
-    elif request.method in ["PUT", "PATCH"]:
+    def put(self, request, pk):
+        contact = self.get_object(pk)
         serializer = UpdateContactSerializer(
-            contact, data=request.data, partial=request.method == "PATCH"
+            contact, data=request.data, partial=False
         )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == "DELETE":
+    def patch(self, request, pk):
+        contact = self.get_object(pk)
+        serializer = UpdateContactSerializer(
+            contact, data=request.data, partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        contact = self.get_object(pk)
         contact.soft_delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(["GET"])
-def search_contact(request):
-    name = request.query_params.get("name")
-    phone_number = request.query_params.get("phone_number")
+class SearchContact(APIView):
+    def get(self, request):
+        name = request.query_params.get("name")
+        phone_number = request.query_params.get("phone_number")
 
-    contacts = Contact.objects.filter(
-        is_deleted=False,
-        name__icontains=name,
-        phone_number__icontains=phone_number,
-    )
-    serializer = ContactSerializer(contacts, many=True)
-    return Response(serializer.data)
+        contacts = Contact.objects.filter(
+            is_deleted=False,
+            name__icontains=name,
+            phone_number__icontains=phone_number,
+        )
+        serializer = ContactSerializer(contacts, many=True)
+        return Response(serializer.data)
 
 
-@api_view(["GET"])
-def export_contacts(request):
-    contacts = Contact.objects.filter(is_deleted=False)
-    serializer = ContactSerializer(contacts, many=True)
+class ExportContacts(APIView):
+    def get(self, request):
+        contacts = Contact.objects.filter(is_deleted=False)
+        serializer = ContactSerializer(contacts, many=True)
 
-    # Sending email using Celery task
-    send_contact_list_email.delay(serializer.data)
+        # Sending email using Celery task
+        send_contact_list_email.delay(serializer.data)
 
-    return Response(
-        {"message": "Exporting contact list. An email will be sent shortly."},
-        status=status.HTTP_200_OK,
-    )
+        return Response(
+            {
+                "message": "Exporting contact list. An email will be sent shortly."
+            },
+            status=status.HTTP_200_OK,
+        )
